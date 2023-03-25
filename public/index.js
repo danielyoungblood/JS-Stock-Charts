ac9b5a9d6c21d20f384fa9dd67df3f7eeb5c9c2f
daniel <danielyknechtli@gmail.com>
2023-03-24 17:41:04 -0700

    added comments

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
 async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+  //document.querySelector gets the canvas HTML element so we can refer to the canvas HTML element by the name timeChartCanvas, this HTML element is on line 15
+  const timeChartCanvas = document.querySelector("#time-chart"); //timeChartCanvas is a variable, it refers to the canvas HTML element that has the id of time-chart
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 }
 
-main()
+main();
